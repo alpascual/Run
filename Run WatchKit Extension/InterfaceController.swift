0f86923a45abb1a8ccb26577715ad08e25d68942
Al Pascual <alpascual@gmail.com>
2014-12-12 18:49:18 -0800

    enable the watch on the run app

---
+++ Run WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,38 @@
+//
+//  InterfaceController.swift
+//  Run WatchKit Extension
+//
+//  Created by Albert Pascual on 12/12/14.
+//
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+        NSLog("%@ awakeWithContext", self)
+    }
+
+    @IBOutlet weak var runTimer: WKInterfaceTimer!
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        NSLog("%@ will activate", self)
+        
+        //self.runTimer.setDate(<#date: NSDate#>)
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        NSLog("%@ did deactivate", self)
+        super.didDeactivate()
+    }
+
+}
